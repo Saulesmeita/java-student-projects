26d55a1fa8a6795d928655fd3dc20a54eeaef813
Anna Terehova <anna.terehova@gmail.com>
2019-10-15 18:56:07 +0300

    OOP basics, user input research.

---
+++ Encoder.java
@@ -0,0 +1,17 @@
+package lv.javaguru.lesson3;
+
+public class Encoder {
+
+    public void printSymbol (char character) {
+
+        int code = (int) character;
+        System.out.println(character + " => " + code);
+    }
+
+    public void printSymbol (int code) {
+
+        char character = (char) code;
+        System.out.println(code + " => " + character);
+    }
+
+}
---
+++ EncoderDemo.java
@@ -0,0 +1,36 @@
+package lv.javaguru.lesson3;
+
+import java.util.Scanner;
+
+public class EncoderDemo {
+
+    public static void main(String[] args) {
+
+        int code;
+        char character;
+
+        Encoder myEncoder = new Encoder();
+
+        Scanner userInput = new Scanner(System.in);
+
+        int work;
+
+        System.out.println("Программа Энкодер преобразует символы Юникода в натуральные числа от 0 до 65535 и наоборот");
+
+        do {
+            System.out.println();
+            System.out.println("Введите целое число от 0 до 65535 или символ:");
+
+            if (userInput.hasNextInt()) {
+                code = userInput.nextInt();
+                myEncoder.printSymbol(code);
+            } else if (userInput.hasNextLine()) {
+                character = userInput.next().charAt(0);
+                myEncoder.printSymbol(character);
+            }
+            System.out.println("Нажмите 1, чтобы продолжить, и 0, чтобы выйти из программы");
+            work = userInput.nextInt();
+        } while (work != 0);
+
+    }
+}
