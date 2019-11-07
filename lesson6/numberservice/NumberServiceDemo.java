1e82a38b7c6336843a5c25a9869e9d02a81548fc
Anna Terehova <anna.terehova@gmail.com>
2019-11-07 16:13:53 +0200

    Some new test cases added: start == finish, |start - finish| == 1, 2.

--- lesson6/numberservice/NumberServiceDemo.java
+++ lesson6/numberservice/NumberServiceDemo.java
@@ -13,11 +13,14 @@ public class NumberServiceDemo {
         System.out.println(ns.rangeSum(0, 10)); //55
         System.out.println(ns.rangeSum(10, 0)); //55
         System.out.println(ns.rangeSum(-5, 5)); //0
+        System.out.println(ns.rangeSum(3, 3)); //3
+        System.out.println(ns.rangeSum(0, 0)); //0
+        System.out.println();
 
         System.out.println(ns.rangeEvenCount(4, 8)); //3
-        System.out.println(ns.rangeEvenCount(8, 4)); //3
+        System.out.println(ns.rangeEvenCount(10, 4)); //4
         System.out.println(ns.rangeEvenCount(4, 9)); //3
-        System.out.println(ns.rangeEvenCount(9, 4)); //3
+        System.out.println(ns.rangeEvenCount(11, 4)); //4
         System.out.println(ns.rangeEvenCount(3, 8)); //3
         System.out.println(ns.rangeEvenCount(3, 9)); //3
         System.out.println(ns.rangeEvenCount(0, 8)); //4
@@ -25,11 +28,17 @@ public class NumberServiceDemo {
         System.out.println(ns.rangeEvenCount(-4, -9)); //3
         System.out.println(ns.rangeEvenCount(4, 0)); //2
         System.out.println(ns.rangeEvenCount(4, -4)); //4
+        System.out.println(ns.rangeEvenCount(0, 0)); //0
+        System.out.println(ns.rangeEvenCount(2, 2)); //1
+        System.out.println(ns.rangeEvenCount(4, 3)); //1
+        System.out.println(ns.rangeEvenCount(-3, -3)); //0
+        System.out.println(ns.rangeEvenCount(2, 2)); //1
+        System.out.println();
 
         System.out.println(ns.rangeEvenCount1(4, 8)); //3
-        System.out.println(ns.rangeEvenCount1(8, 4)); //3
+        System.out.println(ns.rangeEvenCount1(10, 4)); //4
         System.out.println(ns.rangeEvenCount1(4, 9)); //3
-        System.out.println(ns.rangeEvenCount1(9, 4)); //3
+        System.out.println(ns.rangeEvenCount1(11, 4)); //4
         System.out.println(ns.rangeEvenCount1(3, 8)); //3
         System.out.println(ns.rangeEvenCount1(3, 9)); //3
         System.out.println(ns.rangeEvenCount1(0, 8)); //4
@@ -37,5 +46,10 @@ public class NumberServiceDemo {
         System.out.println(ns.rangeEvenCount1(-4, -9)); //3
         System.out.println(ns.rangeEvenCount1(4, 0)); //2
         System.out.println(ns.rangeEvenCount1(4, -4)); //4
+        System.out.println(ns.rangeEvenCount1(0, 0)); //0
+        System.out.println(ns.rangeEvenCount1(2, 2)); //1
+        System.out.println(ns.rangeEvenCount1(4, 3)); //1
+        System.out.println(ns.rangeEvenCount1(-3, -3)); //0
+        System.out.println(ns.rangeEvenCount1(2, 2)); //1
     }
 }
